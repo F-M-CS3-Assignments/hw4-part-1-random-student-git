4f2e944de044adae0f12f725a4d50a9921080b83
random-student-git <apaul1@fandm.edu>
2025-03-21 12:37:35 -0400

    Add files via upload

--- bdc.h
+++ bdc.h
@@ -1,11 +1,84 @@
-
 #include <vector>
-#include <string>
+#include <cassert>
+#include <iostream>
+#include <set>
+#include <algorithm>
+#include <climits>
 
 using namespace std;
 
-// Although pass by reference would probably be more efficient
-// for one or both of these methods, it is not a substantial difference
-// since the vectors used in this program are not very large.
-string vec_to_string(vector<int> v);
-vector<int> biggest_divisible_conglomerate(vector<int> input);
+// pass by reference is probably more efficient
+vector<int> bdc_helper(const vector<int>& inputVector);
+vector<int> longest_vector(const vector<vector<int>>& candidates);
+string vec_to_string(const vector<int>& vec);
+
+
+vector<int> biggest_divisible_conglomerate(vector<int> inputVector){
+    sort(inputVector.begin(),inputVector.end());  //sort vector before starting
+    return bdc_helper(inputVector);
+}
+
+int find_next_dividend_position(const vector<int>& inputVector,int dividend,int pos){
+    int next_dividend_position=-1;   //declare index
+    for (int j=pos;j<inputVector.size();j++){   
+        if (min(abs(inputVector[j]),abs(dividend))!=0){   // prevent division by 0
+            if (max(abs(inputVector[j]),abs(dividend))%min(abs(inputVector[j]),abs(dividend))==0){
+                // this divides the larger of the two numbers by the smaller automatically
+                // also, using absolute value allows division with negative integers (via their magnitude) as well
+                next_dividend_position=j; // update our index for the next dividend position
+                break;
+            }
+        }
+    }
+    return next_dividend_position; 
+    //returning -1 means that there's no next dividend, but returning anything else mean that there is
+}
+
+vector<int> bdc_helper(const vector<int>& inputVector){
+    if (inputVector.size()==0||inputVector.size()==1){ // Basecase: when there's nothing else for subsetting
+        return inputVector;
+    }
+    vector<vector<int>> candidates;
+    for (int i=0;i<inputVector.size()-1;i++){
+        vector<int> L={inputVector[i]};
+        int j=find_next_dividend_position(inputVector,inputVector[i],i + 1);
+        if (j==-1){
+            if (!candidates.empty())
+                return longest_vector(candidates);
+            else
+                return L;
+        }
+        vector<int> Rin(inputVector.begin()+j,inputVector.end());
+        vector<int> R=bdc_helper(Rin);
+        vector<int> cand_v(L.size()+R.size());
+        merge(L.begin(),L.end(),R.begin(),R.end(),cand_v.begin());
+        candidates.push_back(cand_v);
+    }
+    return longest_vector(candidates);
+}
+
+vector<int> longest_vector(const vector<vector<int>>& candidates){
+    int index=0; //default index
+    int max_size=candidates[0].size(); // default vector size (see next line's comment)
+    vector<int> longest_vec=candidates[0]; // basically, candidates[0] is default vector
+    for (int i=1;i<candidates.size();i++){
+        if (max_size<candidates[i].size()){ // for updating everything if a new maximum vector size is found
+            index=i;
+            longest_vec=candidates[i];
+            max_size=candidates[i].size();
+        }
+    }
+    return longest_vec;
+}
+
+string vec_to_string(const vector<int>& vec){
+    string s="{";
+    for (int i=0;i<vec.size();i++){
+        s+=to_string(vec[i]); //concatenate the next element as string
+        if (i!=vec.size()-1){ //as long as we aren't on the last element, keep using commas
+            s+=",";
+        }
+    }
+    s+="}";
+    return s;
+}
--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
-
-#include "bdc.h"
-
 #include <vector>
 #include <cassert>
 #include <iostream>
 #include <set>
+#include <algorithm>
+#include <climits>
+#include "bdc.h"
 
+using namespace std;
 
 int main() {
 
 	// Test 1 (example from assignment description)
 	vector<int> values = {28, 22, 7, 2, 8, 14, 24, 56};
 	vector<int> ans = biggest_divisible_conglomerate(values);
-	cout << "input: " << vec_to_string(values) << endl;
-	cout << "output: " << vec_to_string(ans) << endl << endl;
+	// cout << "input: " << vec_to_string(values) << endl;
+	// cout << "output: " << vec_to_string(ans) << endl << endl;
 	set<int> answerSet(ans.begin(), ans.end());
 	set<int> soln = {56, 14, 7, 28};
 	bool first = (answerSet == soln);
@@ -25,13 +26,78 @@ int main() {
 	// Test 2
 	values = {10, 5, 3, 15, 20};
 	ans = biggest_divisible_conglomerate(values);
-	cout << "input: " << vec_to_string(values) << endl;
-	cout << "output: " << vec_to_string(ans) << endl << endl;
+	// cout << "input: " << vec_to_string(values) << endl;
+	// cout << "output: " << vec_to_string(ans) << endl << endl;
 	soln = {20, 5, 10};
 	set<int> answerSet2(ans.begin(), ans.end());
 	assert(answerSet2 == soln);
 
 	// write your own tests here!
 
+    values = {3, 3, 6, 9};
+	ans = biggest_divisible_conglomerate(values);
+	// cout << "input: " << vec_to_string(values) << endl;
+	// cout << "output: " << vec_to_string(ans) << endl << endl;
+	soln = {3, 3, 6};
+	set<int> answerSet3(ans.begin(), ans.end());
+	assert(answerSet3 == soln);
+
+    values = {};
+    ans = biggest_divisible_conglomerate(values);
+    // cout << "input: " << vec_to_string(values) << endl;
+    // cout << "output: " << vec_to_string(ans) << endl << endl;
+    soln = {};
+	set<int> answerSet4(ans.begin(), ans.end());
+	assert(answerSet4 == soln);
+
+    values = {0};
+    ans = biggest_divisible_conglomerate(values);
+    // cout << "input: " << vec_to_string(values) << endl;
+    // cout << "output: " << vec_to_string(ans) << endl << endl;
+    soln = {0};
+	set<int> answerSet5(ans.begin(), ans.end());
+	assert(answerSet5 == soln);
+    
+    values = {69};
+    ans = biggest_divisible_conglomerate(values);
+    // cout << "input: " << vec_to_string(values) << endl;
+    // cout << "output: " << vec_to_string(ans) << endl << endl;
+    soln = {69};
+	set<int> answerSet6(ans.begin(), ans.end());
+	assert(answerSet6 == soln);
+
+    values = {1,INT_MAX};
+    ans = biggest_divisible_conglomerate(values);
+    // cout << "input: " << vec_to_string(values) << endl;
+    // cout << "output: " << vec_to_string(ans) << endl << endl;
+    soln = {1,INT_MAX};
+	set<int> answerSet7(ans.begin(), ans.end());
+	assert(answerSet7 == soln);
+
+	values = {-7,7,-14,14};
+    ans = biggest_divisible_conglomerate(values);
+    // cout << "input: " << vec_to_string(values) << endl;
+    // cout << "output: " << vec_to_string(ans) << endl << endl;
+    soln = {-14,-7,7,14};
+    set<int> answerSet8(ans.begin(), ans.end());
+	assert(answerSet8 == soln);
+    
+    values = {-1,0,1};
+    ans = biggest_divisible_conglomerate(values);
+    // cout << "input: " << vec_to_string(values) << endl;
+    // cout << "output: " << vec_to_string(ans) << endl << endl;
+    soln = {-1,1};
+    set<int> answerSet9(ans.begin(), ans.end());
+	assert(answerSet9 == soln);
+    
+    values = {7,11,17,3};
+    ans = biggest_divisible_conglomerate(values);
+    // cout << "input: " << vec_to_string(values) << endl;
+    // cout << "output: " << vec_to_string(ans) << endl << endl;
+    soln = {3};
+    set<int> answerSet10(ans.begin(), ans.end());
+	assert(answerSet10 == soln);
+
+    cout<<"ALL TESTS PASSED!"<<endl;
 	return 0;
 }
